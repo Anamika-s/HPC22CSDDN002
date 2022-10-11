bcabda98e9d667620ac3d8c2f73976417f33896f
Anamika Sawhney <anamika.sawhney22@gmail.com>
2022-10-11 12:50:36 +0530

    C# Day1

---
+++ 11thOctober_C#/First.cs
@@ -0,0 +1,13 @@
+ // Basic structure of C# program
+ using System;    // import package name , here System is a library , namespace
+  // min 1 class has to be there
+ class Program
+
+ {
+  static void Main()
+{
+     Console.Write("Hello");
+   
+}
+
+ }
